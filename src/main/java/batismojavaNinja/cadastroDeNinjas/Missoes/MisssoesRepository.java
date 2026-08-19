0dce75197a762ab103a78b454ee9c60aa7bf5273
Bruno <179547556+Bruno92-prog@users.noreply.github.com>
2026-08-19 15:18:29 -0300

    feat: Criado NinjaService,NinjaRepository e MissoesRepository a ultima como interfaxe extendendo JpaRpository

---
+++ src/main/java/batismojavaNinja/cadastroDeNinjas/Missoes/MisssoesRepository.java
@@ -0,0 +1,6 @@
+package batismojavaNinja.cadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MisssoesRepository extends JpaRepository<MissoesModel, Long> {
+}
