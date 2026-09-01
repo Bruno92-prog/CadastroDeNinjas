d14cd768f303c0b6215ad00eaf16e8e89955313a
Bruno <179547556+Bruno92-prog@users.noreply.github.com>
2026-09-01 19:27:29 -0300

    feat: Inclui DTO como abstração para minha entidade na camada de sevice e controller

--- src/main/java/batismojavaNinja/cadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/batismojavaNinja/cadastroDeNinjas/Ninjas/NinjaService.java
@@ -9,6 +9,9 @@ import java.util.Optional;
 @Service
 public class NinjaService {
 
+    // TODO CODIGO ABAIXO É O ORIGINAL DO SERVICE ANTES DE SER FEITO O DTO E MAPPER
+
+    /*
     @Autowired
     private NinjaRepository ninjaRepository;
 
@@ -43,4 +46,53 @@ public class NinjaService {
         return null;
     }
 
+
+     */
+
+    // depois com DTO e MAPPER
+
+    @Autowired
+    private NinjaRepository ninjaRepository;
+
+    //Criando mapper e construtor
+    private NinjaMapper ninjaMapper;
+
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
+        this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
+    }
+
+    // Listar todos os meus ninjas
+
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    // Listar ninjas por id
+    public NinjaModel listarNinjasPorId(Long id){
+        Optional <NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.orElse(null);
+    }
+
+    // CRIAR UM NOVO NINJA
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja =  ninjaMapper.map(ninjaDTO);
+        ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
+
+    }
+
+    // Deletar o ninja
+    public void deletarNinjaPorId(Long id){
+        ninjaRepository.deleteById(id);
+    }
+
+    // Atualizar um  ninja
+    public NinjaModel atualizarNinja(Long id,NinjaModel ninjaAtualizado){
+        if(ninjaRepository.existsById(id)){
+            ninjaAtualizado.setId(id);
+            return ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
 }
